68fb655ae18b72ae44c5da5ef965b30e9e274c30
Akash Wagh <Akash.Wagh@esds.co.in>
2025-06-26 12:07:19 +0530

    get dev

--- src/config/url_config.js
+++ src/config/url_config.js
@@ -1,1 +1,5 @@
-export const BASEURL = 'http://localhost/expense-manager-api/public';
+// Local
+// export const BASEURL = 'http://localhost/expense-manager-api/public';
+
+// Development
+export const BASEURL = 'https://dev.famrut.com/expense-manager-api/public';
